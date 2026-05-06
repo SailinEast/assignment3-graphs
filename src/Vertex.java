1702a2e65890fbdc8eb8dd2d14cf0fd4433d2950
Sail <anasboranbaev@gmail.com>
2026-05-06 21:17:00 +0500

    feat(vertex): implemented Vertex class

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,19 @@
 public class Vertex {
+    private static int idGen = 0;
+    private final int id;
+
+    public Vertex() {
+        this.id = idGen++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{" +
+            "id=" + id +
+            '}';
+    }
 }
